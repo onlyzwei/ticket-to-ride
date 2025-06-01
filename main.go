1269a80c2d430eb34858ce3c200d469f2e478779
Thiago <thiagomeloper@hotmail.com>
2025-05-31 23:16:01 -0300

    Estruturando Endpoints

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"ticket-to-ride\pkg\api"
 )
 
 func main() {
+	http.HandleFunc("/api/game/create", api.CreateGameHandler)
+	http.HandleFunc("/api/game/state", api.GetGameStateHandler)
+	http.HandleFunc("/api/game/play", api.PlayMoveHandler)
+	http.HandleFunc("/api/game/buy-train-card", api.BuyTrainCardHandler)
+	http.HandleFunc("/api/game/swap-tickets", api.SwapTicketHandler)
+
+	log.Println("Servidor iniciado em :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

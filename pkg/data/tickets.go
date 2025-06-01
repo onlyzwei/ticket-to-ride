c8ee0446057947cf439532ed38225cbb91a93251
Thiago Melo <82834145+Thiagomelosz@users.noreply.github.com>
2025-05-31 23:33:00 -0300

    Adaptando logica do jogo

---
+++ pkg/data/tickets.go
@@ -0,0 +1,35 @@
+package data
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"ticket-to-ride/pkg/game"
+)
+
+func Tickets() ([]game.Ticket, error) {
+	f, err := os.Open("./pkg/data/USA/tickets.csv")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	var toRead bool
+	var tickets []game.Ticket
+	for scanner.Scan() {
+		if !toRead {
+			toRead = true
+			continue
+		}
+		line := scanner.Text()
+		f := strings.Split(line, ",")
+		x, y, scoreStr := f[0], f[1], f[2]
+		score, err := strconv.Atoi(scoreStr)
+		if err != nil {
+			return nil, err
+		}
+		tickets = append(tickets, game.Ticket{X: game.City(x), Y: game.City(y), Value: score})
+	}
+	return tickets, nil
+}
